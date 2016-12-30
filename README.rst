fac0fe8cc8ca045314e258fca33b53e30f8e0e82
orweis <orweis@gmail.com>
2016-12-30 19:35:51 +0200

    Package for PyPi

---
+++ README.rst
@@ -0,0 +1,67 @@
+winpcapy
+========
+
+| A Modern Python wrapper for WinPcap
+| Access WinPcap through ctypes.
+
+Based on Massimo Ciani’s WinPcapy (https://code.google.com/p/winpcapy/)
+
+Usage
+-----
+
+Quick packet live log printer
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. code:: python
+
+    >>> from winpcapy import WinPcapUtils
+    # run on the first Ethernert interface and print a log for each packet
+    >>> WinPcapUtils.capture_on_and_print("*Ethernet*")
+    16:05:49,624258 len:199
+    16:05:49,685950 len:60
+    16:05:49,686022 len:54
+    16:05:49,767311 len:66
+    16:05:49,819156 len:66
+    16:05:50,052113 len:92
+    16:05:50,128862 len:60
+
+Easy Packet live callback
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. code:: python
+
+    from winpcapy import WinPcapUtils
+
+    # Example Callback function to parse IP packets
+    def packet_callback(win_pcap, param, header, pkt_data):
+        # Assuming IP (for real parsing use modules like dpkt)
+        ip_frame = pkt_data[14:]
+        # Parse ips
+        src_ip = ".".join([str(ord(b)) for b in ip_frame[0xc:0x10]])
+        dst_ip = ".".join([str(ord(b)) for b in ip_frame[0x10:0x14]])
+        print("%s -> %s" % (src_ip, dst_ip))
+
+    WinPcapUtils.capture_on("*Ethernet*", packet_callback)
+
+Device/Interface enumeration
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. code:: python
+
+    >>> from winpcapy import WinPcapDevices
+    # run on the first Ethernert interface and print a log for each packet
+    >>> WinPcapDevices.list_devices()
+    {'\\Device\\NPF_{0A78B7C8-F023-1337-1337-84D448AA5126}': 'Microsoft',
+     '\\Device\\NPF_{2997B9BB-AA53-1337-1337-B862F874271C}': 'Microsoft',
+     '\\Device\\NPF_{C2EAA982-F851-1337-1337-B8D2A9BCE406}': 'Intel(R) Ethernet Connection I218-LM',
+     '\\Device\\NPF_{EAF47DBE-5B49-1337-1337-BD059E02666B}': 'Microsoft'}
+     
+     # Itearte over devices (in memory), with full details access
+    >>> with WinPcapDevices() as devices:
+    ...     for device in devices:
+    ...         print device.name, device.description, device.flags ,device.addresses.contents.netmask.contents.sa_family
+    ...         
+    "\Device\NPF_{0A78B7C8-F023-1337-1337-84D448AA5126} Microsoft 0 0"
+    "\Device\NPF_{C2EAA982-F851-1337-1337-B8D2A9BCE406} Intel(R) Ethernet Connection I218-LM 0 0"
+    "\Device\NPF_{EAF47DBE-5B49-1337-1337-BD059E02666B} Microsoft 0 0"
+    "\Device\NPF_{2997B9BB-AA53-1337-1337-B862F874271C} Microsoft 0 0"
---
+++ setup.py
@@ -0,0 +1,83 @@
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+###############################################################################
+
+NAME = "winpcapy"
+PACKAGES = ["winpcapy"]
+META_PATH = os.path.join("winpcapy", "__init__.py")
+KEYWORDS = ["winpcapy", "pcap", "winpcap", "Packet capture", "tcpdump"]
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+    "Operating System :: Microsoft :: Windows",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: System :: Networking :: Monitoring",
+]
+INSTALL_REQUIRES = []
+
+###############################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+VERSION = find_meta("version")
+URI = find_meta("uri")
+LONG = read("README.rst")
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        description=find_meta("description"),
+        license=find_meta("license"),
+        url=URI,
+        version=VERSION,
+        author=find_meta("author"),
+        author_email=find_meta("email"),
+        maintainer=find_meta("author"),
+        maintainer_email=find_meta("email"),
+        keywords=KEYWORDS,
+        long_description=LONG,
+        packages=PACKAGES,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+    )
